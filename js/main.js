e0a5cc0ebb60f7ca6924e05f52307b8c3ebd7af3
Slawa <slawapocht4@yandex.ru>
2021-05-22 23:06:45 +0400

    working application with bugs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const btnsList = document.querySelectorAll('.caluclator__btn');
+const calculatorInput = document.querySelector('.calculator__input');
+let string = '';
+
+btnsList.forEach(btn => {
+    btn.addEventListener('click', handlerBtn);
+});
+
+function handlerBtn(evt) {
+    const target = evt.target;
+    createVomiting(target.dataset.btnValue);
+}
+
+function createVomiting(data) {
+		if (data === '=') {
+			renderInInput(getResult(string));
+			string = '';
+		}else {
+				string += data;
+				renderInInput(string);
+		}
+}
+
+function getResult(vomiting) {
+	let result = eval(vomiting);
+	return(result);
+}
+
+function renderInInput(str) {
+	calculatorInput.value = str;
+}
